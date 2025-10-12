b8199f2fa4081fd17f6e58f40feb1ccbc1fdc750
Hans Hsieh <539874+hanshsieh@users.noreply.github.com>
2025-10-12 20:42:07 +0800

    Change Ditto timestamp field to include timezone and exactly 3 decimal places

--- src/main/java/org/freedger/services/ditto/InstantAdapter.java
+++ src/main/java/org/freedger/services/ditto/InstantAdapter.java
@@ -7,14 +7,14 @@ import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
 public class InstantAdapter extends TypeAdapter<Instant> {
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS");
+    // The formatter ensures the output is in ISO8601 format with exactly3 decimal places
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
     private static final ZoneOffset ZONE_OFFSET = ZoneOffset.UTC;
 
     @Override
@@ -34,10 +34,9 @@ public class InstantAdapter extends TypeAdapter<Instant> {
             return null;
         }
         try {
-            final var localDateTime = LocalDateTime.parse(in.nextString(), FORMATTER);
-            return OffsetDateTime.of(localDateTime, ZONE_OFFSET).toInstant();
+            return Instant.parse(in.nextString());
         } catch (DateTimeParseException e) {
-            throw new JsonParseException("Invalid date format. Expected format: yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSS", e);
+            throw new JsonParseException("Invalid ISO8601 date format", e);
         }
     }
 }
